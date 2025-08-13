52b0c2496348dcd8f40e2ed7bc65b746365ec8c3
vikas.bhardwaj <vikas.bhardwaj@telekom-digital.com>
2025-08-13 14:56:48 +0530

    feat: lint staged, husky pre commit & commit-msg implemented

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier/recommended";
+import tseslint from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +14,23 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  js.configs.recommended,
+  prettier,
+
+  // TS ESLint rules
+  {
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "no-unused-vars": "off", // disable base rule for TS files
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
